5e1ad9048dfb5e0be1df0471663a0b363055b02d
bogdanovkp <bogdanovkp@it2g.ru>
2019-03-02 15:53:12 +0400

    main project

---
+++ MeetRoomWeb/meet-room-web/src/main/java/ru/sb/meetroom/config/MeetRoomApplication.java
@@ -0,0 +1,11 @@
+package ru.sb.meetroom.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetRoomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeetRoomApplication.class, args);
+    }
+}
---
+++ MeetRoomWeb/web/src/main/java/ru/sb/meetroom/MeetroomApplication.java
@@ -0,0 +1,13 @@
+package ru.sb.meetroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetroomApplication.class, args);
+	}
+
+}
